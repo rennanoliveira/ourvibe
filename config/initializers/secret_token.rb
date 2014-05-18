8e9a9160719c49846b12bd76a8cfa225c50d76d1
Rennan Oliveira <renmcoliver@gmail.com>
2014-05-17 22:29:51 -0300

    Initial commit (Empty project)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ourvibe::Application.config.secret_token = 'cf9f90d11d51530f0903ecc4b860175fb8d21a105aa0a5350185d8dad0912679fb70901bb7cdf80f57fd9d09d91b3d43aa23286e0f2731b78701781237403826'
